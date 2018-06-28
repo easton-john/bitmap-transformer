27f4c5f76522b286f820608987004a42eb8db897
Easton Gorishek <ieaston26@gmail.com>
2018-06-28 10:19:07 -0700

    second test passes

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -15,7 +15,7 @@ describe('bitmap header', () => {
     });
     // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
 
-    it.only('has correct specs', () => {
+    it('has correct specs', () => {
         // TODO: read the wiki spec docs to figure out what these values should be.
         // You don't need to change this test, you need to put the correct values into
         // '../lib/bitmap-constants'
