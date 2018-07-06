8aab09245b8982d9e5b4376da53902683d0b0289
Easton Gorishek <ieaston26@gmail.com>
2018-07-06 15:57:48 -0700

    inverted transformer done

--- test/streaming-bitmap-transformer.test.js
+++ test/streaming-bitmap-transformer.test.js
@@ -1,19 +1,23 @@
 const assert = require('assert');
 const fs = require('fs');
 const StreamingBitmapTransformer = require('../lib/streaming-bitmap-transformer');
-const invert = require('../lib/invert-transformer');
+const { invert } = require('../lib/invert-transformer');
+
+const readFile = file => fs.readFileSync(file);
 
 describe('Streaming Bitmap Transformer', () => {
     const source = './test/test-bitmap.bmp';
     const output = './test/inverted-bitmap.bmp';
 
     it('Streams and inverts bitmap', () => {
-        StreamingBitmapTransformer.create(source)
+        return StreamingBitmapTransformer.create(source)
             .then(bitmapTransformer => {
-                bitmapTransformer.transform(invert, output);
+                return bitmapTransformer.transform(invert, output)
+                    .then(() => {
+                        const expected = readFile('./test/inverted-expected.bmp');
+                        const actual = readFile(output);
+                        assert.deepEqual(actual, expected);
+                    });
             });
-            // .then(() => {
-            //     assert.deepEqual(fs.readFile('./test/inverted-bitmap.bmp'), fs.readFile('./test/test-bitmap.bmp'));
-            // });
     });
 });
